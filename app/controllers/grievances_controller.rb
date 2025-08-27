46bdbce187809b6a438fe852d41885099378ec53
Naing Oo Lwin <naingoolwin@gmail.com>
2025-08-27 15:33:48 +0900

    added grievance index new and show

--- app/controllers/grievances_controller.rb
+++ app/controllers/grievances_controller.rb
@@ -9,7 +9,24 @@ class GrievancesController < ApplicationController
     @grievance = Grievance.new
   end
 
+  def create
+    @grievance = Grievance.new(grievance_params)
+    @grievance.partnership = Partnership.find(params[:partnership_id])
+    @grievance.user = current_user
+    if @grievance.save
+      redirect_to grievance_path(@grievance)
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
   def show
     @grievance = Grievance.find(params[:id])
+  end
+
+  private
+
+  def grievance_params
+    params.require(:grievance).permit(:topic, :feeling, :situation, :intensity_scale)
   end
 end
