304175602f66dd93df2b4839fc2fb64f7c157489
Vince <jiangliujp@gmail.com>
2025-08-27 11:36:42 +0900

    fixed partnership show page and fixed navbar path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def weekday_mixed_abbr(date)
+    map = {
+      0 => "Su", # Sunday
+      1 => "M",  # Monday
+      2 => "Tu", # Tuesday
+      3 => "W",  # Wednesday
+      4 => "Th", # Thursday
+      5 => "F",  # Friday
+      6 => "Sa"  # Saturday
+    }
+    map[date.wday]
+  end
 end
