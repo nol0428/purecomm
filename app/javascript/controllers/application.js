18d5015ce927b8072b8a6d15e8e2cd7ab7cbd3fb
Paul Miyamoto <mfsaru@me.com>
2025-08-28 21:37:06 +0900

    Stimulus: simplify to eagerLoadControllersFrom; remove manual registrations

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,7 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+// Configure Stimulus development experience here, if you want.
+// application.debug = false
 
 export { application }
