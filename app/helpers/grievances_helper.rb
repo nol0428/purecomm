932a68f20714e918bc6532e27da8a916b126b59f
Vince <jiangliujp@gmail.com>
2025-08-28 15:29:15 +0900

    fixed grievance show

---
+++ app/helpers/grievances_helper.rb
@@ -0,0 +1,10 @@
+module GrievancesHelper
+  def grievance_feelings_options
+    [
+      ["Tired", "tired"],
+      ["Sad", "sad"],
+      ["Upset", "upset"],
+      ["Anxious", "anxious"]
+    ]
+  end
+end
