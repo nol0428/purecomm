d7de0665624340a0e9554bb9cf0f237cfedc45d8
Paul Miyamoto <mfsaru@me.com>
2025-09-02 15:16:41 +0900

    fix(importmap): explicitly pin talk_scroll_controller

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+pin "controllers/talk_scroll_controller", to: "controllers/talk_scroll_controller.js"
