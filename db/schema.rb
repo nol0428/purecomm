f206dc00d31d9183608d1ce96d603f4f14f51c7c
Harry Salacuri <harry-moto@hotmail.com>
2025-09-02 10:18:17 +0900

    Removedprofilepicfromgrievances

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_01_033756) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_09_01_051028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
