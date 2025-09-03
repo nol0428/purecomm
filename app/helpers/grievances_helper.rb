3a9837689028d2b7fc2d69ab06e21b3daa7cdd1c
Vince <jiangliujp@gmail.com>
2025-09-03 16:04:54 +0900

    changed grievance drop show page

--- app/helpers/grievances_helper.rb
+++ app/helpers/grievances_helper.rb
@@ -7,4 +7,23 @@ module GrievancesHelper
       ["Anxious", "anxious"]
     ]
   end
+
+  FEELING_MAP = {
+    "tired"  => "Tired",
+    "sad"    => "Sad",
+    "upset"  => "Upset",
+    "anxious"=> "Anxious"
+  }.freeze
+
+  def grievance_feeling_key(grievance)
+    grievance.feeling.to_s.downcase
+  end
+
+  def grievance_feeling_label(grievance)
+    FEELING_MAP[grievance_feeling_key(grievance)] || grievance.feeling.to_s.titleize
+  end
+
+  def grievance_feeling_icon(grievance)
+    "#{grievance_feeling_key(grievance)}.svg"
+  end
 end
